9fe7aa3a79ecb9c4aa353178e84885174a01656f
Sergey Makarov <sgmakgg@gmail.com>
2023-04-27 18:34:41 +0300

    hw

---
+++ shop/iShop.js
@@ -0,0 +1,35 @@
+let iShopVariable = React.createClass({
+
+    displayName: 'iShopComponentDisplayName',
+
+    render: function(){
+
+        let products = [];
+        let tableFirstRow = 
+            React.DOM.tr({className:'tableTop'},
+                React.DOM.th({className:'productName'}, 'Name'),
+                React.DOM.th({className:'productPrice'}, 'Price'),
+                React.DOM.th({className:'productPhoto'}, 'Photo'),
+                React.DOM.th({className:'productQuantity'}, 'Warehouse'));
+
+        products = this.props.products.map(product =>
+            React.DOM.tr({key:product.code, className:'tableRow'},
+                React.DOM.td({className:'productName'}, product.name),
+                React.DOM.td({className:'productPrice'}, product.price),
+                React.DOM.td({className:'productPhoto'}, 
+                    React.DOM.img({src:product.imageURL, alt:product.name, className:'rowImage'})),
+                React.DOM.td({className:'productQuantity'}, product.warehouseQuantity))
+            );
+// 
+        products.splice(0, 0, tableFirstRow);
+
+        return React.DOM.div({className:'iShop'},
+            React.DOM.h1( {className: 'ShopName'}, this.props.shopName ),
+            React.DOM.table( {className:'iShopTable'}, products ),
+        );
+    },
+
+});
+
+
+
