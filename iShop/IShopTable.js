9f9f275f4a1ec345d2def5d524a572e9e27683c5
Sergey Makarov <sgmakgg@gmail.com>
2023-04-28 13:35:42 +0300

    props added

--- iShop/IShopTable.js
+++ iShop/IShopTable.js
@@ -1,17 +1,30 @@
-let iShopVariable = React.createClass({
+let iShop = React.createClass({
 
     displayName: 'iShopComponentDisplayName',
 
+    propTypes: {
+        shopName: React.PropTypes.string.isRequired,
+        products: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                name: React.PropTypes.string.isRequired, 
+                price: React.PropTypes.number.isRequired, 
+                imageURL: React.PropTypes.string.isRequired, 
+                warehouseQuantity: React.PropTypes.number.isRequired, 
+                code: React.PropTypes.number.isRequired
+            })
+        )
+    },
+
     render: function(){
 
-        let products = [];
-        let tableFirstRow = 
+        let tableHead = 
             React.DOM.tr({className:'tableHead'},
                 React.DOM.th({className:'productName'}, 'Name'),
                 React.DOM.th({className:'productPrice'}, 'Price'),
                 React.DOM.th({className:'productPhoto'}, 'Photo'),
                 React.DOM.th({className:'productQuantity'}, 'Warehouse'));
 
+        let products = [];
         products = this.props.products.map(product =>
             React.DOM.tr({key:product.code, className:'tableBody'},
                 React.DOM.td({className:'productName'}, product.name),
@@ -20,15 +33,14 @@ let iShopVariable = React.createClass({
                     React.DOM.img({src:product.imageURL, alt:product.name, className:'rowImage'})),
                 React.DOM.td({className:'productQuantity'}, product.warehouseQuantity))
             );
-// 
-        products.splice(0, 0, tableFirstRow);
 
-        return React.DOM.div({className:'iShop'},
+        return React.DOM.div({className:'IShopTable'},
             React.DOM.h1( {className: 'ShopName'}, this.props.shopName ),
-            React.DOM.table( {className:'iShopTable'}, products ),
+            React.DOM.table( {className:'iShopTable'}, 
+                React.DOM.thead({className:'tableHead'}, tableHead),
+                React.DOM.tbody({className:'tableBody'}, products)),
         );
     },
-
 });
 
 
