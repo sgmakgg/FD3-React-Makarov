e64011c089b2d4a615fa81d13869a693e6061831
Sergey Makarov <sgmakgg@gmail.com>
2023-05-10 18:03:34 +0300

    import_data_to_json

--- iShop3/App.js
+++ iShop3/App.js
@@ -3,35 +3,7 @@ import ReactDOM from 'react-dom';
 
 import iShopTable from './components/IShopTable';
 
-class Product{
-    constructor(name, price, imageURL, warehouseQuantity, code) {
-        this.name = name;
-        this.price = price;
-        this.imageURL = imageURL;
-        this.warehouseQuantity = warehouseQuantity;
-        this.code = code;
-    }
-};
-
-let shopProducts = [
-    new Product(
-        'Milk',
-        1.50,
-        'images/milk.png',
-        100,
-        100),
-    new Product(
-        'Olive oil',
-        20.22,
-        'images/olive-oil.png',
-        100500,
-        200),
-    new Product(
-        'Bread',
-        0.25,
-        'images/bread.png',
-        300,
-        300)];
+import shopProducts from './defaultData.json';
 
 const shopName = 'iShop2';
 
