4f9062737791620dd29d9cc7bcfd5e1777dff737
Sergey Makarov <sgmakgg@gmail.com>
2023-05-22 17:21:02 +0300

    mCh

--- iShop3/ProductCardEditableEvents.js
+++ iShop3/ProductCardEditableEvents.js
@@ -2,11 +2,5 @@ import {EventEmitter} from 'events';
 
 let changeCardPropsEvents=new EventEmitter();
 export const isEditing = 'isEditing';
-export const wasEdited = 'wasEdited';
-// в потоке voteEvents будут все события, связанные с голосованием
-// событие "EAnswerClicked" - кликнут вариант ответа, его сэмиттирует VotesAnswer и примет VotesBlock
-// событие "EFreeAnswerTextChanged" - изменён текст свободного ответа, его сэмиттирует VotesAnswer и примет VotesBlock
-// лучше работать не с текстовыми литералами, а объявить переменные с соответствующими значениями:
-// const EAnswerClicked="EAnswerClicked";
 
 export {changeCardPropsEvents};
