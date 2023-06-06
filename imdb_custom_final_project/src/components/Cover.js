9d59206e926094b31e4aeaac7725b39e5d6435a4
Sergey Makarov <sgmakgg@gmail.com>
2023-06-06 19:20:20 +0300

    cover_page

--- imdb_custom_final_project/src/components/Cover.js
+++ imdb_custom_final_project/src/components/Cover.js
@@ -5,7 +5,6 @@ import iPhone from '../images/iPhone.svg';
 import movieDB from '../images/movieDB.svg';
 
 export const Cover = () => {
-
     return (
         <div className='CoverPage' >
             <img className='IPhone'
@@ -24,15 +23,16 @@ export const Cover = () => {
                         height:'30%',
                         width:'30%',
                         }} src={movieDB} alt='MovieDB_picture'/>
-            <input className='StartButton' type='button' value={'Try\xa0it\xa0now'}
-                   style={{
+            <input className='StartButton'
+                       type='button'
+                       value={'Try\xa0it\xa0now'}
+                style={{
                        position: "absolute",
-                       left: '5vw',
+                       left: '12vw',
                        top: '45vh',
                        height:'10%',
                        width:'15%',
-                   }} src={movieDB} alt='MovieDB_picture'/>
+                       }} src={movieDB} alt='MovieDB_picture'/>
         </div>
     );
-
 };
