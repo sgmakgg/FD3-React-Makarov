cbfba354111af879d376bba2f4a8fc3d0fb27b5b
Sergey Makarov <sgmakgg@gmail.com>
2023-06-07 23:00:00 +0300

    grid_init

--- moviedb/src/components/Search.js
+++ moviedb/src/components/Search.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
+import Grid from '@mui/material/Unstable_Grid2';
+import Item from '@mui/material/Unstable_Grid2';
+
 import './Search.css'
-import {Grid} from "@mui/material";
 
 export const Search = () => {
     return (
             <Grid className='SearchPage'  container>
-                <Grid item xs={3}>
-                    <div style={{backgroundColor: 'red', height:'100%'}}></div>
+                <Grid xs={3}
+                      display="flex"
+                      justifyContent="center"
+                      alignItems="center">
+                    <Item>1</Item>
                 </Grid>
-                <Grid item xs={9} zeroMinWidth>
-                    <div style={{backgroundColor: 'green', height:'100%'}}></div>
+                <Grid xs={9}
+                      display="flex"
+                      justifyContent="center"
+                      alignItems="center">
+                    <Item>2</Item>
                 </Grid>
             </Grid>
-        // </div>
     );
 };
