3190324bd00d37ac13cf99f984c7c17e40eb58dd
Sergey Makarov <sgmakgg@gmail.com>
2023-06-09 20:45:44 +0300

    pagination

--- moviedb/src/components/Main.js
+++ moviedb/src/components/Main.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Grid from '@mui/material/Unstable_Grid2';
 import Item from '@mui/material/Unstable_Grid2';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Button } from '@mui/material';
+import {Button, Pagination, TextField} from '@mui/material';
 
 import './Main.css'
 
@@ -19,25 +19,58 @@ const theme = createTheme({
 
 const Main = () => {
 
+    const[top250MoviesOn, setTop250MoviesOn] = useState(false);
+    const[pagesCount, setPagesCount] = useState(0);
+    const[pageNumber, setPageNumber] = useState(1);
+
+    const turnOnTop250Movies = () => {
+        setTop250MoviesOn(true);}
+
+    const cbPagesCount = (count) =>{
+        setPagesCount(count);
+    }
+    const handleChange  = (EO, value) =>{
+        setPageNumber(value);
+    }
+
     return (
-            <Grid className='SearchPage'  container>
+            <Grid className='MainPage'  container>
                 <Grid xs={3} xl={1}
                       display="flex"
                       justifyContent="center"
                       alignItems="top">
                     <Item>
                         <ThemeProvider theme={theme}>
-                            <Button color="secondary" size="large">
+                            <Button color="secondary" size="large" onClick={turnOnTop250Movies}>
                                 {'Top\xa0250\xa0Movies'}
                             </Button>
                         </ThemeProvider>
                     </Item>
                 </Grid>
-                <Grid xs={9} xl={11}
+                <Grid container xs={9} xl={11}
                       display="flex"
                       justifyContent="center"
                       alignItems="center">
-                    <Top250Movies/>
+                        <Grid xs={12} display="flex"
+                              justifyContent="center"
+                              alignItems="center">
+                                <TextField
+                                    id="standard-search"
+                                    label="Search field"
+                                    type="search"
+                                    variant="standard"
+                                    sx={{width:'70%', paddingBottom:'1vh'}}/>
+                        </Grid>
+                        <Grid xs={12} sx={{minHeight:'90vh'}}>
+                            {(top250MoviesOn)&&<Top250Movies cbPagesCount={cbPagesCount}
+                                                             elementsPerPage={10}
+                                                             pageNumber={pageNumber}/>}
+                        </Grid>
+                        <Grid xs={12} display="flex"
+                              justifyContent="center"
+                              alignItems="center">
+                            <Pagination count={pagesCount} onChange={handleChange}/>
+                        </Grid>
                 </Grid>
             </Grid>
     );
--- moviedb/src/components/Top250Movies.js
+++ moviedb/src/components/Top250Movies.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './Top250Movies.css';
 
@@ -10,7 +10,22 @@ import MovieCard from "./MovieCard";
 
 const data = JSON.parse(localStorage.getItem('Top250Movies')).items;
 
-const Top250Movies = () =>{
+const Top250Movies = ({cbPagesCount, elementsPerPage, pageNumber}) =>{
+
+    const[responseData, setResponseData] = useState(data);
+    const[pageSize, setPageSize] = useState(elementsPerPage);
+    const[page, setPage] = useState(pageNumber);
+    const[currentPageArr, setCurrentPageArr] = useState(null);
+
+    useEffect(
+        ()=>{
+            if(responseData.length !== 0){
+                countPages();
+            }
+            setPage(pageNumber);
+            currentPage();
+        },[responseData, page, pageNumber]);
+
     // const { isLoading, error, data } = useQuery(
     //     'top250movies',
     //     () =>
@@ -18,15 +33,29 @@ const Top250Movies = () =>{
     //         'https://imdb-api.com/en/API/Top250Movies/k_ckcqw367'
     //       ).then((response) => response.json())
     //   );
-
+    //
     // if(!isLoading){
     //     console.log({isLoading, error, data});
     //     localStorage.setItem('Top250Movies', JSON.stringify(data));
     // }
 
+    const countPages = () =>{
+        let res = responseData.length/pageSize;
+        cbPagesCount(res);
+    }
+
+    const currentPage = () => {
+        let firstIndex = ((page - 1) * pageSize);
+        let lastIndex = firstIndex + pageSize;
+        let res = responseData.slice(firstIndex, lastIndex);
+        setCurrentPageArr(res);
+    }
+
+
     return(
+        (currentPageArr !== null) &&
         <Grid container rowSpacing={2} columnSpacing={2} disableEqualOverflow>
-            {data.map((movie, index) => (
+            {currentPageArr.map((movie, index) => (
                 <Grid xs={12} md={6} xl={3}
                       key={index}
                       display="flex"
